5e79460825d9ca9f9c4e4bd77f26f5581769810e
Fakrudeen Ali T <fafakrudeen@gmail.com>
2024-02-21 01:54:24 +0530

    /message API

--- src/main/java/com/fakru/dockerjenkins/DockerJenkinsApplication.java
+++ src/main/java/com/fakru/dockerjenkins/DockerJenkinsApplication.java
@@ -2,9 +2,17 @@ package com.fakru.dockerjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerJenkinsApplication {
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return String.valueOf(Math.random());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DockerJenkinsApplication.class, args);
